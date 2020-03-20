5b49358799e8dd4639585c1889dfe12cc3a5c756
RODRIGO SILVA PERES <rodrigo.peres@gsw.com.br>
2020-03-20 11:23:21 -0300

    POC1 - Rodrigo Peres

--- Controllers/CarrinhoController.cs
+++ Controllers/CarrinhoController.cs
@@ -12,7 +12,7 @@ namespace Carrinho.Controllers
 {
     public class CarrinhoController : Controller
     {
-        private readonly CarrinhoContext _context;
+
         private readonly CarrinhoDAO _carrinhoDAO;
         private readonly SetorDAO _setorDAO;
 
@@ -20,7 +20,6 @@ namespace Carrinho.Controllers
 
         public CarrinhoController()
         {
-            _context = new CarrinhoContext();
             _carrinhoDAO = new CarrinhoDAO();
             _setorDAO = new SetorDAO();
             _produtoDAO = new ProdutoDAO();
@@ -34,22 +33,24 @@ namespace Carrinho.Controllers
 
         public async Task<IActionResult> Add()
         {
-           
+
             ViewBag.SetorId = new SelectList(_setorDAO.SetoresEnumerable(), "Id", "Nome");
             ViewBag.IdCarrinho = _carrinhoDAO.NovoCarrinho("Em Andamento").Id;
             return View(await _produtoDAO.Produtos());
         }
+
+        [HttpPost]
         public async Task<IActionResult> AddCarrinho(int? id, int idCarrinho, int qtd)
         {
-            if (id == null )
+            if (id == null)
             {
                 return NotFound();
             }
             var carrinho = await _carrinhoDAO.getCarrinhoById(idCarrinho);
             var produto = await _produtoDAO.getProdutoById(id);
-            carrinho.ListaProdutos.Add(new CarrinhoProdutos() { Produto = produto , QtdProduto = qtd});
+            carrinho.ListaProdutos.Add(new CarrinhoProdutos() { Produto = produto, QtdProduto = qtd });
             _carrinhoDAO.Update(carrinho);
-           
+
             if (produto == null)
             {
                 return NotFound();
@@ -57,14 +58,36 @@ namespace Carrinho.Controllers
             return Json(produto);
         }
 
-        public async Task<IActionResult> FinalizarCompra(int id){
-            System.Console.WriteLine(id);
+        public async Task<IActionResult> FinalizarCompra(int id)
+        {
+
+            var carrinho = await _carrinhoDAO.getCarrinhoById(id);
+            carrinho.EstadoCompra = "Confirmacao";
+            _carrinhoDAO.Update(carrinho);
+            ViewBag.IdCarrinho = id;
+
+            return View(carrinho.ListaProdutos);
+        }
+
+        public async Task<IActionResult> Confirmar(int id){
             var carrinho = await _carrinhoDAO.getCarrinhoById(id);
             carrinho.EstadoCompra = "Finalizada";
             _carrinhoDAO.Update(carrinho);
             return View();
         }
-        public async void EnterromperCompra(int idCarrinho){
+        public async Task<IActionResult> RemoverCarrinho(int? id, int idCarrinho , int qtd){
+
+            var carrinho = await _carrinhoDAO.getCarrinhoById(idCarrinho);
+            var produto = carrinho.ListaProdutos.Find(p => p.Produto.Id == id);
+            System.Console.WriteLine(carrinho.Id);
+            System.Console.WriteLine(produto.Produto.Id);
+            carrinho.ListaProdutos.Remove(produto);
+            _carrinhoDAO.Update(carrinho);
+            return Json(produto.Produto);
+
+        }
+        public async void EnterromperCompra(int idCarrinho)
+        {
             var carrinho = await _carrinhoDAO.getCarrinhoById(idCarrinho);
             carrinho.EstadoCompra = "Enterrompida";
             _carrinhoDAO.Update(carrinho);
--- DAO/CarrinhoDAO.cs
+++ DAO/CarrinhoDAO.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
 using Carrinho.Dados;
 using Carrinho.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Carrinho.DAO
 {
@@ -12,7 +13,8 @@ namespace Carrinho.DAO
             _context = new CarrinhoContext();
         }
 
-        public async Task<CarrinhoCompras> getCarrinhoById(int? id) => await _context.Carrinho.FindAsync(id);
+        public async Task<CarrinhoCompras> getCarrinhoById(int? id) => await _context.Carrinho.Include(c => c.ListaProdutos).ThenInclude(cp => cp.Produto).FirstOrDefaultAsync(c => c.Id == id);
+        
         public CarrinhoCompras NovoCarrinho(string estado) {
              var carrinho = _context.Add(new CarrinhoCompras(){EstadoCompra = estado});
              _context.SaveChanges();
